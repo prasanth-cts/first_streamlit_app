16d9c71512d1f3cf2c58227ef4c456146950e855
prasanth-cts <137046490+prasanth-cts@users.noreply.github.com>
2023-06-19 14:08:30 +0530

    Update streamlit_app.py
    Changed upper case to lower case of Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Heatlhy Dinner')
 streamlit.header('Breakfast Menu')
-Streamlit.text('Omega 3 and Blueberry oatmeal')
-Streamlit.text('Kale, Spinach & Rocket juice')
-Streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket juice')
+streamlit.text('Hard-Boiled Free-Range Egg')
